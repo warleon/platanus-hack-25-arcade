1fc0b43e1d7884b7b1b2ee44e67af8e148e9044f
warleon <51892390+warleon@users.noreply.github.com>
2025-10-31 09:10:12 -0500

    Update game.js

--- game.js
+++ game.js
@@ -3,30 +3,80 @@ const config = {
   width: 800,
   height: 600,
   backgroundColor: "#000000",
+  pixelArt: true,
   scene: {
+    preload: preload,
     create: create,
     update: update,
+  },
+  phisics: {
+    default: "arcade",
+    arcade: { debug: true },
   },
 };
 
 const game = new Phaser.Game(config);
 
+const SPRITE_SHEET =
+  "data:image/webp;base64,UklGRkQAAABXRUJQVlA4TDgAAAAvP8ADAB8gFkz8kXLoHJyQgNjjvz12iAIBIkiJCKHu5j/GHY0AYbbRJCc5yUO4b0T/Y9uqVAH+Ag==";
+
+// anim states: idle, walk, attack, die
+const IDLE = "idle";
+const WALK = "walk";
+const ATTACK = "attack";
+const DIE = "die";
 // Game variables
-let gameOver = false;
 let graphics;
 const resources = [];
 
+function preload() {
+  this.load.spritesheet("test", SPRITE_SHEET, {
+    frameWidth: 16,
+    frameHeight: 16,
+  });
+}
 function create() {
   const scene = this;
   graphics = this.add.graphics();
 
-  for (let i = 0; i < 3; i++) {
-    resources.push({ x: 0.1, y: i * 0.25 + 0.25 });
-    resources.push({ x: 0.9, y: i * 0.25 + 0.25 });
+  for (let i = 0; i < 2; i++) {
+    resources.push({ x: 0.1, y: i * 0.33 + 0.33, size: 0.05 });
+
+    resources.push({ x: 0.9, y: i * 0.33 + 0.33, size: 0.05 });
   }
 
   // Keyboard input
   this.input.keyboard.on("keydown", onInput);
+
+  scene.anims.create({
+    key: IDLE,
+    frames: scene.anims.generateFrameNumbers("test", { start: 0, end: 0 }),
+    frameRate: 2,
+    repeat: -1,
+  });
+  scene.anims.create({
+    key: WALK,
+    frames: scene.anims.generateFrameNumbers("test", { start: 1, end: 1 }),
+    frameRate: 2,
+    repeat: -1,
+  });
+  scene.anims.create({
+    key: ATTACK,
+    frames: scene.anims.generateFrameNumbers("test", { start: 2, end: 2 }),
+    frameRate: 2,
+    repeat: -1,
+  });
+  scene.anims.create({
+    key: DIE,
+    frames: scene.anims.generateFrameNumbers("test", { start: 3, end: 3 }),
+    frameRate: 2,
+    repeat: -1,
+  });
+
+  scene.add
+    .sprite(config.width / 2, config.height / 2, "test")
+    .play("idle")
+    .setScale(8);
 
   playTone(this, 440, 0.1);
 }
@@ -34,8 +84,6 @@ function create() {
 function onInput(event) {}
 
 function update(_time, delta) {
-  if (gameOver) return;
-  endGame(this);
   drawGame();
 }
 
@@ -44,12 +92,14 @@ function drawGame() {
 
   const colors = [0x00ff00, 0x0000ff, 0xffff00];
   resources.forEach((res, index) => {
-    drawSquare(res.x, res.y, 0.1, colors[index % colors.length]);
+    drawRect(colors[index % colors.length], res.x, res.y, res.size);
   });
-}
-
-function endGame(scene) {
-  gameOver = true;
+  // UI
+  drawRect(0xff0000, 0.2, 0.9, 0.4, 0.2);
+  drawRect(0xff0000, 0.8, 0.1, 0.4, 0.2);
+  // bases
+  drawRect(0x00ff00, 0.2, 0.1, 0.4, 0.2);
+  drawRect(0x00ff00, 0.8, 0.9, 0.4, 0.2);
 }
 
 function restartGame(scene) {
@@ -77,8 +127,12 @@ function playTone(scene, frequency, duration) {
   oscillator.stop(audioContext.currentTime + duration);
 }
 
-function drawSquare(x, y, size, color) {
+function drawRect(color, x, y, width, height = null) {
+  if (height === null) {
+    height = width;
+  }
   graphics.fillStyle(color, 1);
-  const w = size * config.width;
-  graphics.fillRect(x * config.width - w / 2, y * config.height - w / 2, w, w);
+  const w = width * config.width;
+  const h = height * config.width;
+  graphics.fillRect(x * config.width - w / 2, y * config.height - h / 2, w, h);
 }
